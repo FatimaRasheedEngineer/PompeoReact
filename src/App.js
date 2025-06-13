94c0b535463702cf81362b26c035028c0e6dd20b
FatimaRasheedEngineer <fatimarasheed277@gmail.com>
2025-06-14 01:55:45 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
